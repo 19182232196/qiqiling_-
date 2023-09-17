6cae3ced8a14ac4932089bfdcbdf2c3143ffd3d9
Nenhung <83660804+19182232196@users.noreply.github.com>
2023-09-17 18:08:42 +0800

    Add files via upload

---
+++ Test/Test.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Enter a number: ");
+//        int n=in.nextInt();
+//        if (n>2){
+//            System.out.println("The number is greater than 2");
+//        }else {
+//            System.out.println("The number is less than or equal to 2");
+//            }
+//        _________________________________________________________________________
+//        int Q=1;
+//        if(Q==1){
+//            System.out.println("Yes");
+//        }
+//        _________________________________________________________________________
+//        boolean Q=true;
+//        boolean W=false;
+//        boolean E=true;
+//        if(Q){
+//            System.out.println("Yes");
+//        }
+//        if(W){
+//            System.out.println("Yes");
+//        }
+//        if(E){
+//            System.out.println("Yes");
+//        }
+//        if(Q&&W){
+//            System.out.println("Yes");
+//        }
+//        if(Q||W){
+//            System.out.println("Yes");
+//        }
+//        __________________________________________________________________________
+//        Scanner Q=new Scanner(System.in);
+//        System.out.println("Enter a number: ");
+//        int W=Q.nextInt();
+//        if(W>2) {
+//            System.out.println("Yes");
+//        }else {
+//            System.out.println("No");
+//        }
+//
+//        __________________________________________________________________________
+//        Scanner Q=new Scanner(System.in);
+//        System.out.println("Tips: Enter the whole number for the money paid");
+//        int W=Q.nextInt();
+//        if(W>=600){
+//            System.out.println("Payment successful");
+//        }else {
+//            System.out.println("Payment unsuccessful");
+//        }
+//        __________________________________________________________________________
+//        Scanner Q=new Scanner(System.in);
+//        System.out.println("Enter a number:  (1-100)");
+//        int W=Q.nextInt();
+//        if(W>=1&&W<=100) {
+//            if (W % 2 == 1) {
+//                System.out.println("Right");
+//            } else {
+//                System.out.println("Left");
+//            }
+//       }
+//       __________________________________________________________________________
+//         Scanner Q=new Scanner(System.in);
+//         System.out.println("Enter a number: ");
+//         int W=Q.nextInt():
+//         if(W>=95&&W<=100){
+//             System.out.println("A");
+//         }else if(W>=85&&W<=94){
+//             System.out.println("B");
+//         }else if(W>=75&&W<=84){
+//             System.out.println("C");
+//         }else if(W>=65&&W<=74){
+//             System.out.println("D");
+//         }else if(W>=55&&W<=64){
+//             System.out.println("E");
+//         }else {
+//             System.out.println("F");
+//         }
+//        __________________________________________________________________________
+
+    }
+}
