fa368412b66d657ede33a2e1aeb73b57e4f724ff
Nenhung <83660804+19182232196@users.noreply.github.com>
2023-09-19 11:53:51 +0800

    Add files via upload

--- Test-c/nodejs/test.js
+++ Test-c/nodejs/test.js
@@ -1,7 +1,24 @@
-const fs = require('fs');
-fs.readFile('./demo.txt', 'utf-8', function (err, dataStr) {
-    if (err) {
-        console.log('No'+err);
-    }
-    console.log('ok'+dataStr);
-})
+// const fs = require('fs');
+// fs.readFile('./demo.txt', 'utf-8', function (err, dataStr) {
+    // if (err) {
+        // console.log('No'+err);
+    // }
+    // console.log('ok'+dataStr);
+// })
+//** */
+//** */
+// fs.writeFile('./demo.txt', 'hello world', function (err) {
+    // if (err) {
+        // console.log(err);
+    // }
+    // console.log('写入');
+// })
+//** */
+//** */
+//------------------------------------------------------------------
+const events = require('events');
+const myevents = new events.EventEmitter();
+myevents.on('say', function (name) { 
+    console.log('hello'+name);
+});
+myevents.emit('say', 'world');
