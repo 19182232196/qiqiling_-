84a49adc4f8c2813b60212bc3cf6fe0747edc465
Nenhung <83660804+19182232196@users.noreply.github.com>
2023-10-18 12:04:51 +0800

    Add files via upload

---
+++ test-d/1project/demo3/src/main/java/com/el/javaBean/Child.java
@@ -0,0 +1,14 @@
+package com.el.javaBean;
+
+public class Child {
+
+	String name = "通过Person获得Child的属性";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
