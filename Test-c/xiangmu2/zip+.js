1c6470d69493eb953b479a23f638393f73ed7cf6
Nenhung <83660804+19182232196@users.noreply.github.com>
2023-10-25 10:49:43 +0800

    Delete Test-c/xiangmu2 directory

--- Test-c/xiangmu2/zip+.js
+++
@@ -1,6 +0,0 @@
-const fs=require('fs');
-const zlib=require('zlib');
-fs.createReadStream('./demo.txt.gz')
-    .pipe(zlib.createGzip())
-    .pipe(fs.createWriteStream('./demo.txt'));
-console.log('ok');
