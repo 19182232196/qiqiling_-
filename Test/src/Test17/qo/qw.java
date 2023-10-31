e1aaedca6338d5e341172cf01db29fc6788e72b6
Nenhung <83660804+19182232196@users.noreply.github.com>
2023-10-31 09:43:05 +0800

    Add files via upload

---
+++ Test/src/Test17/qo/qw.java
@@ -0,0 +1,65 @@
+package com.qo;
+
+public class qw {
+    public static void main(String args[]){
+//    tD();
+
+//    tD1();
+//    tD2();
+    }
+
+//    public static void tD(){
+//        TD t1=new TD("t1");
+//        TD t2=new TD("t2");
+//        TD t3=new TD("t3");
+//        TD t4=new TD("t4");
+//        t1.start();
+//        t2.start();
+//        t3.start();
+//        t4.start();
+//    }
+//    static class TD extends Thread implements Runnable{
+//        public TD(String n){
+//            super(n);
+//        }
+//        int i=100;
+//        @Override
+//        public  void run(){
+//            while (i>0){
+//                System.out.println(Thread.currentThread().getName()+" "+(i--));
+//            }
+//        }
+//    }
+
+//    public static void tD1(){
+//        TD t1=new TD("t1");
+//        TD t2=new TD("t2");
+//        TD t3=new TD("t3");
+//        TD t4=new TD("t4");
+//        t1.start();
+//        t2.start();
+//        t3.start();
+//        t4.start();
+//    }
+//    public static void tD2(){
+//        TD t=new TD();
+//        new Thread(t,"t1").start();
+//        new Thread(t,"t2").start();
+//        new Thread(t,"t3").start();
+//        new Thread(t,"t4").start();
+//    }
+//    static class TD extends Thread implements Runnable{
+//        int t=100;
+//
+//        public TD(String n) {
+//            super(n);
+//        }
+//
+//        @Override
+//        public void run(){
+//            while (t>0){
+//                System.out.println(Thread.currentThread().getName()+" "+(t--));
+//            }
+//        }
+//    }
+}
