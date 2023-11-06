28cb3b5eebe180799f1a719b67663885d450cc2e
Nenhung <83660804+19182232196@users.noreply.github.com>
2023-11-06 18:41:58 +0800

    Delete Test-b/test7 directory

--- Test-b/test7/car.py
+++
@@ -1,7 +0,0 @@
-car_price={"微型车":10,"小型车":20,"中型车":30,"大型车":40,"紧凑型车":50,"跑车":60,"SUV":70,"MPV":80,"房车":90}
-car_input=input("请输入车型").strip()
-sch_price=car_price.get(car_input,"nothing")
-if sch_price!="nothing":
-    print(sch_price)
-else:
-    print("车型不存在")
--- Test-b/test7/xs.py
+++
@@ -1,9 +0,0 @@
-net=input('网站名')
-if net=='baidu':
-    print('www.baidu.com')
-elif net=='google':
-    print('www.google.com')
-elif net=='sogou':
-    print('www.sogou.com')
-else:
-    print('请输入正确的网站名')
