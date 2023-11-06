5c187e7a93b8745888414e5a29434538b97b3205
Nenhung <83660804+19182232196@users.noreply.github.com>
2023-11-06 18:41:21 +0800

    Delete Test-b/chepai.py

--- Test-b/chepai.py
+++
@@ -1,7 +0,0 @@
-switcher={"MA":"Fuzhou","SH":"Shanghai","SZ":"Shenzhen","CQ":"Chongqing","HD":"Hangzhou","HB":"Hubei","HN":"Hunan"}
-car_input=input("请输入车牌号：")
-result=switcher.get(car_input, "nuthing")
-if  result=="nuthing":
-    print("输入错误")
-else:
-    print(result)
