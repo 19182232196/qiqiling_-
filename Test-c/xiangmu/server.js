cf0c90b62a1bf5b949b31193846ef0c8c11e3ed0
Nenhung <83660804+19182232196@users.noreply.github.com>
2023-11-10 23:09:23 +0800

    Add files via upload

--- Test-c/xiangmu/server.js
+++ Test-c/xiangmu/server.js
@@ -1,6 +1,13 @@
-const http = require('http');
+const express = require('express');
 const mysql = require('mysql2');
 const querystring = require('querystring');
+
+const app = express();
+const server = app.listen(8080, () => {
+    console.log('Server running on port 8080');
+});
+
+app.use(express.static('public'));
 
 const pool = mysql.createPool({
     host: 'localhost',
@@ -13,69 +20,64 @@ const pool = mysql.createPool({
     queueLimit: 0
 });
 
-const server = http.createServer((req, res) => {
-    if (req.method === 'POST') {
-        let postVal = '';
-        req.on('data', (chunk) => {
-            postVal += chunk;
-        });
-        req.on('end', () => {
-            let formVal = querystring.parse(postVal);
-            let userName = formVal.userName;
-            let userPwd = formVal.userPwd;
+app.post('/register', (req, res) => {
+    let postVal = '';
+    req.on('data', (chunk) => {
+        postVal += chunk;
+    });
+    req.on('end', () => {
+        let formVal = querystring.parse(postVal);
+        let userName = formVal.userName;
+        let userPwd = formVal.userPwd;
 
-            pool.getConnection((err, connection) => {
+        pool.getConnection((err, connection) => {
+            if (err) {
+                res.status(500).send('数据库连接失败');
+                return;
+            }
+
+            connection.query('INSERT INTO user (userName, userPwd) VALUES (?, ?)', [userName, userPwd], (err, results, fields) => {
+                connection.release();
+
                 if (err) {
-                    res.writeHead(500, { 'Content-Type': 'text/html;charset=utf-8' });
-                    res.write('数据库连接失败');
-                    res.end();
-                    return;
-                }
-
-                if (req.url === '/register') {
-                    connection.query('INSERT INTO user (userName, userPwd) VALUES (?, ?)', [userName, userPwd], (err, results, fields) => {
-                        connection.release();
-
-                        if (err) {
-                            res.writeHead(500, { 'Content-Type': 'text/html;charset=utf-8' });
-                            res.write('注册失败');
-                            res.end();
-                        } else {
-                            res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
-                            res.write('注册成功');
-                            res.end();
-                        }
-                    });
-                } else if (req.url === '/login') {
-                    connection.query('SELECT * FROM user WHERE userName=? AND userPwd=?', [userName, userPwd], (err, results, fields) => {
-                        connection.release();
-
-                        if (err) {
-                            res.writeHead(500, { 'Content-Type': 'text/html;charset=utf-8' });
-                            res.write('登录失败');
-                            res.end();
-                        } else {
-                            if (results.length > 0) {
-                                res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
-                                res.write('登录成功');
-                                res.end();
-                            } else {
-                                res.writeHead(401, { 'Content-Type': 'text/html;charset=utf-8' });
-                                res.write('用户名或密码不正确');
-                                res.end();
-                            }
-                        }
-                    });
+                    res.status(500).send('注册失败');
+                } else {
+                    res.status(200).send('<a href="/index.html">登录</a>');
                 }
             });
         });
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/html;charset=utf-8' });
-        res.write('Page Not Found');
-        res.end();
-    }
+    });
 });
 
-server.listen(8080, () => {
-    console.log('Server running on port 8080');
+app.post('/login', (req, res) => {
+    let postVal = '';
+    req.on('data', (chunk) => {
+        postVal += chunk;
+    });
+    req.on('end', () => {
+        let formVal = querystring.parse(postVal);
+        let userName = formVal.userName;
+        let userPwd = formVal.userPwd;
+
+        pool.getConnection((err, connection) => {
+            if (err) {
+                res.status(500).send('数据库连接失败');
+                return;
+            }
+
+            connection.query('SELECT * FROM user WHERE userName=? AND userPwd=?', [userName, userPwd], (err, results, fields) => {
+                connection.release();
+
+                if (err) {
+                    res.status(500).send('登录失败');
+                } else {
+                    if (results.length > 0) {
+                        res.status(200).send('<a href="/index.html">登录</a>');
+                    } else {
+                        res.status(401).send('用户名或密码不正确');
+                    }
+                }
+            });
+        });
+    });
 });
