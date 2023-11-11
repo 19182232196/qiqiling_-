6fd8876dbefc0cd0added042f65d89e355c0f41d
Nenhung <83660804+19182232196@users.noreply.github.com>
2023-11-11 15:00:42 +0800

    Add files via upload

--- Test-c/xiangmu/server.js
+++ Test-c/xiangmu/server.js
@@ -4,10 +4,13 @@ const querystring = require('querystring');
 const ejs = require('ejs');
 const cors = require('cors'); // 引入 cors 模块
 const bodyParser = require('body-parser');
+const path = require('path');
+
 
 
 const app = express();
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(cors()); // 使用 cors 中间件
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -151,3 +154,69 @@ app.post('/performDatabaseAction', (req, res) => {
     });
 });
 
+app.get('/students', (req, res) => {
+    pool.getConnection((err, connection) => {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        }
+
+        connection.query('SELECT * FROM user', (err, results) => {
+            connection.release();
+
+            if (err) {
+                res.status(500).send(err);
+                return;
+            }
+
+            // 渲染学生列表页面，将学生数据传递给模板
+            res.render('students', { students: results });
+        });
+    });
+});
+
+// 编辑学生信息
+app.post('/editStudent', (req, res) => {
+    const { id, userName, userPwd } = req.body;
+
+    pool.getConnection((err, connection) => {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        }
+
+        connection.query('UPDATE user SET userName = ?, userPwd = ? WHERE id = ?', [userName, userPwd, id], (err, results) => {
+            connection.release();
+
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                // 成功后重定向回学生列表页面
+                res.redirect('/students');
+            }
+        });
+    });
+});
+
+// 删除学生
+app.post('/deleteStudent', (req, res) => {
+    const { id } = req.body;
+
+    pool.getConnection((err, connection) => {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        }
+
+        connection.query('DELETE FROM user WHERE id = ?', [id], (err, results) => {
+            connection.release();
+
+            if (err) {
+                res.status(500).send('删除学生失败');
+            } else {
+                // 成功后重定向回学生列表页面
+                res.redirect('/students');
+            }
+        });
+    });
+});
